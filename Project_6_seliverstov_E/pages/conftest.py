4572591bec0f4af6bf0fbbd223029bebcf83eb0f
EugenSeliverstov <eugenseliverstov@gmail.com>
2024-10-18 12:12:32 +0200

    Update conftest.py

--- Project_6_seliverstov_E/pages/conftest.py
+++ Project_6_seliverstov_E/pages/conftest.py
@@ -5,9 +5,9 @@ from selenium import webdriver
 @pytest.fixture(scope="class")
 def init_driver(request):
     # Инициализация драйвера
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
+    chrome_options = Options() 
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install())
     # driver = webdriver.Chrome()
     driver.implicitly_wait(10)
     driver.maximize_window()
